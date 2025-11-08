32a820ff933f86baac9bb3a1a37e37aef799869e
Daisy <p25cs0012@iitj.ac.in>
2025-11-08 17:44:18 +0530

    feat(user-service): add full auth with models, controllers, and routes

---
+++ user-service/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// user-service/routes/userRoutes.js
+import express from 'express'
+const router = express.Router()
+import { registerUser, loginUser } from '../controllers/userController.js'
+
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+
+export default router
